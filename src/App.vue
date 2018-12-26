946928fe48e05a711d02d4fdf11f188ca0041f4f
Rob Neville <robertneville73@gmail.com>
2018-12-26 11:08:26 -0500

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <iframe id="ytplayer" type="text/html" width="400" height="200"
+  src="https://www.youtube.com/embed/vwTJgWdYF_0?autoplay=1&start=33&end=39&controls=0&fs=0&iv_load_policy=3&modestbranding=1"
+  frameborder="0"></iframe>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// iIhNEgBc66A homer scream
+// vwTJgWdYF_0&start=33&end=40 supplies
+// https://www.youtube.com/watch?v=llq3iuSaL1w&start=10&end=14 spongebob oh no
+// https://www.youtube.com/watch?v=GnC1_zVA3-g regular show, holy crap it's real
+// https://www.youtube.com/watch?v=z6oK4oUFWvY&start=11&end=14 sheldon, that's it then, I'm dead
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
