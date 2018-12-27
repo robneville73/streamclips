8c332cef0cfa7016eac2b3698cca96aed2e68038
Rob Neville <robertneville73@gmail.com>
2018-12-27 14:59:14 -0500

    more videos

--- src/videos.js
+++ src/videos.js
@@ -24,6 +24,154 @@ const videos = {
       video_id: 'z6oK4oUFWvY',
       options: { autoplay: 1, start: 11, end: 14 }
     }
+  ],
+  bighit: [
+    {
+      name: 'if it bleeds, we can kill it',
+      video_id: 'eNr0WXQ3Ho4',
+      options: { autoplay: 1, start: 6, end: 10 }
+    },
+    {
+      name: 'dodge this',
+      video_id: 'HQtIearBLAE',
+      options: { autoplay: 1, start: 3, end: 7 }
+    },
+    {
+      name: 'kung pow stomach plug',
+      video_id: '941z56i7QJE',
+      options: { autoplay: 1, start: 74, end: 84 }
+    },
+    {
+      name: 'jerhico',
+      video_id: 'KNAgFhh1ji4',
+      options: { autoplay: 1, start: 62, end: 68 }
+    },
+    {
+      name: 'predator minigun',
+      video_id: 'wgzxSr6l9Y4',
+      options: { autoplay: 1, start: 69, end: 81 }
+    },
+    {
+      name: 'naval nuke',
+      video_id: 'FKbtnviUfHc',
+      options: { autoplay: 1, start: 0, end: 11 }
+    }
+  ],
+  ouch: [
+    {
+      name: 'mark on my face',
+      video_id: 'sVSfcWUxu_Q',
+      options: { autoplay: 1, start: 48, end: 55 }
+    },
+    {
+      name: 'family guy hurt knee',
+      video_id: 'Y9j3heYZAk8',
+      options: { autoplay: 1, start: 0, end: 9 }
+    },
+    {
+      name: 'which one of yall just kicked me',
+      video_id: 'vbuHC6RjbtY',
+      options: { autoplay: 1, start: 154, end: 161 }
+    },
+    {
+      name: 'that really hurts austin powers',
+      video_id: 'pIwLJtqoxBs',
+      options: { autoplay: 1, start: 14, end: 21 }
+    },
+    {
+      name: 'stop blowing holes in my ship',
+      video_id: 'VmXqfMpLvgQ',
+      options: { autoplay: 1, start: 10, end: 15 }
+    },
+    {
+      name: 'i aint got time to bleed',
+      video_id: 'aqDwqdSF6Ec',
+      options: { autoplay: 1 }
+    },
+    {
+      name: 'bike crash',
+      video_id: 'YXWB6ewi1rE',
+      options: { autoplay: 1, start: 87, end: 91 }
+    }
+  ],
+  killshot: [
+    {
+      name: 'iron man vs tank',
+      video_id: 'PjJofEuSA78',
+      options: { autoplay: 1, start: 169, end: 177 }
+    },
+    {
+      name: 'terminated',
+      video_id: 'jMlm9Qhl1gY',
+      options: { autoplay: 1, start: 20, end: 23 }
+    },
+    {
+      name: 'asta la vista baby',
+      video_id: 'OSt11SA16KU',
+      options: { autoplay: 1, start: 133, end: 142 }
+    },
+    {
+      name: 'deadpool 1 shot',
+      video_id: 'CaIf7W06arw',
+      options: { autoplay: 1, start: 132, end: 143 }
+    },
+    {
+      name: 'say hello to my little friend',
+      video_id: 'ro2x8gd2v-M',
+      options: { autoplay: 1, start: 17, end: 21 }
+    },
+    {
+      name: 'ill kill you last, i lied',
+      video_id: 'ro2x8gd2v-M',
+      options: { autoplay: 1, start: 117, end: 125 }
+    },
+    {
+      name: 'this is sparta',
+      video_id: 'ro2x8gd2v-M',
+      options: { autoplay: 1, start: 177, end: 180 }
+    },
+    {
+      name: 'you shall not pass',
+      video_id: 'ro2x8gd2v-M',
+      options: { autoplay: 1, start: 253, end: 259 }
+    }
+  ],
+  derp: [
+    {
+      name: 'potato song',
+      video_id: 'q7uyKYeGPdE',
+      options: { autoplay: 1 }
+    },
+    {
+      name: 'stupid, youre so stupid',
+      video_id: 'KezvwARhBIc',
+      options: { autoplay: 1, start: 76 }
+    },
+    {
+      name: 'my life is potato',
+      video_id: 'QiqqC_fbP1c',
+      options: { autoplay: 1 }
+    },
+    {
+      name: 'i am a potato',
+      video_id: 'Qijju-y_NzI',
+      options: { autoplay: 1 }
+    },
+    {
+      name: 'stupid is as stupid does',
+      video_id: 'tldGgGFe194',
+      options: { autoplay: 1, start: 0, end: 6 }
+    },
+    {
+      name: 'first of all, youre stupid',
+      video_id: 'zSYacmdMPhw',
+      options: { autoplay: 1, start: 37, end: 41 }
+    },
+    {
+      name: 'youre not just wrong, youre stupid',
+      video_id: '40Pvi1XVm_s',
+      options: { autoplay: 1 }
+    }
   ]
 }
 
